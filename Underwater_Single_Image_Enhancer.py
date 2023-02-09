a7b19ca0ccb7ba537cb1f08dc79ebae9c141bd63
uyo9ko <18817658581@163.com>
2023-02-09 20:04:33 +0800

    Add files via upload

---
+++ Underwater_Single_Image_Enhancer.py
@@ -0,0 +1,130 @@
+import streamlit as st
+from PIL import Image,ImageOps
+import numpy as np
+import os
+from UE_code.enhance import enhance_my
+import time
+import base64
+from pathlib import Path
+from nevaluate import nmetrics
+
+
+def resize_image(image, max_size):
+    width, height = image.size
+    if width >= height:
+        ratio = width/height
+        image = image.resize((max_size, int(max_size/ratio)))
+        image = ImageOps.expand(image, (0, int((max_size - image.height) / 2)), fill='white')
+    else:
+        ratio = height/width
+        image = image.resize((int(max_size/ratio), max_size))
+        image = ImageOps.expand(image, (int((max_size - image.width) / 2), 0), fill='white')
+    return image
+
+
+@st.cache
+def cal_metrics(image):
+    uiqm_1, uciqe_1 = nmetrics(np.array(image))
+    return uiqm_1, uciqe_1
+
+
+@st.cache
+def enhance_image(image):
+    enhanced_image = enhance_my(image)
+    uiqm_2, uciqe_2 = nmetrics(np.array(enhanced_image))
+    return enhanced_image, uiqm_2, uciqe_2
+
+
+
+st.set_page_config(page_title="Underwater Single Image Enhancer", page_icon="🤔", layout="wide")
+# st.sidebar.header("Underwater Single Image Enhancer")
+empty_image = Image.open('camera_1.png')
+MAX_SIZE = 500
+empty_image = resize_image(empty_image,MAX_SIZE)
+image = None
+left,middle,right = st.columns([1,4,1])
+with middle:
+    st.title("Underwater Single Image Enhancer")
+    # Define the options for the selection bar
+    options = ['MyModel', 'UDCP', 'GCDP', 'UWCNN','Ucolor']
+    # Create a imgfile uploader widget
+    imgfile = st.file_uploader("Upload an image file", type=["jpg", "jpeg", "png"])
+
+_,col1, col2,_ = st.columns([1,2,2,1])
+
+with col1:
+    st.subheader("Input Image")
+    form = st.form("my_form")
+    tab11,tab12 = form.tabs(['Image','Metrics'])
+    with tab11:
+        placeholder_1 = st.empty()
+        placeholder_1.image(empty_image, caption="Uploaded Image", use_column_width=True)
+    with tab12:
+        placeholder_uiqm_1 = st.empty()
+        placeholder_uciqe_1= st.empty()
+        placeholder_uiqm_1.metric("UIQM", "0.0")
+        placeholder_uciqe_1.metric("UCIQE", "0.0")
+
+    selected_option = form.selectbox("Select an Underwater enhance method :", options, index=0)
+    sb = form.form_submit_button("Enhance")
+
+with col2:  
+    st.subheader("Enhanced Image")
+    tab21,tab22 = st.tabs(['Image','Metrics'])
+    with tab21:
+        placeholder_2 = st.empty()
+        placeholder_2.image(empty_image, caption="Enhanced Image", use_column_width=True)
+    with tab22:
+        placeholder_uiqm_2 = st.empty()
+        placeholder_uciqe_2 = st.empty()
+        placeholder_uiqm_2.metric("UIQM", "0.0","0.0")
+        placeholder_uciqe_2.metric("UCIQE", "0.0","0.0")
+
+if imgfile is not None:
+    image = Image.open(imgfile)
+    with col1:
+        if image:
+            resized_image = resize_image(image, MAX_SIZE)
+            placeholder_1.image(resized_image, caption="Uploaded Image", use_column_width=True)
+            uiqm_1, uciqe_1 = cal_metrics(image)
+            placeholder_uiqm_1.metric("UIQM", f"{uiqm_1:.2f}")
+            placeholder_uciqe_1.metric("UCIQE", f"{uciqe_1:.2f}")
+
+
+if sb:
+    with col2:
+        if image :
+            start_time = time.time()
+            enhanced_image, uiqm_2, uciqe_2 = enhance_image(image)
+            end_time = time.time()
+            st.session_state["enhanced_image"] = enhanced_image
+            st.session_state["uiqm_2"] = uiqm_2
+            st.session_state["uciqe_2"] = uciqe_2
+            st.session_state["enhance_time"] = end_time - start_time
+        else:
+            st.warning("Please upload an image to see the enhanced version")
+
+if "enhanced_image" in st.session_state:
+    with col2:
+        resized_enhanced_image = resize_image(st.session_state["enhanced_image"], MAX_SIZE)
+        placeholder_2.image(resized_enhanced_image, caption="Enhanced Image", use_column_width=True)
+        placeholder_uiqm_2.metric("UIQM", f"{st.session_state['uiqm_2']:.2f}", f"{st.session_state['uiqm_2']-uiqm_1:.2f}")
+        placeholder_uciqe_2.metric("UCIQE", f"{st.session_state['uciqe_2']:.2f}", f"{st.session_state['uciqe_2']-uciqe_1:.2f}")
+        st.success("Image enhanced successfully")
+        st.write(f"Enhance time: {st.session_state['enhance_time']:.2f} seconds")
+        st.session_state["enhanced_image"].save('enhance_img.png')
+
+with col2:
+    with open("enhance_img.png", "rb") as the_file:
+        btn = st.download_button(
+                label="Download image",
+                data=the_file,
+                file_name="enhance_img.png",
+                mime="image/png"
+            )
+
+
+
+
+
+
---
+++ pages/Underwater_Batch_Image_Enhancer.py
@@ -0,0 +1,132 @@
+import streamlit as st
+from PIL import Image,ImageOps
+import numpy as np
+import os
+from UE_code.enhance import enhance_my
+import time
+import base64
+from pathlib import Path
+from nevaluate import nmetrics
+import pandas as pd
+import glob
+import zipfile
+
+
+def resize_image(image, max_size):
+    width, height = image.size
+    if width >= height:
+        ratio = width/height
+        image = image.resize((max_size, int(max_size/ratio)))
+        image = ImageOps.expand(image, (0, int((max_size - image.height) / 2)), fill='white')
+    else:
+        ratio = height/width
+        image = image.resize((int(max_size/ratio), max_size))
+        image = ImageOps.expand(image, (int((max_size - image.width) / 2), 0), fill='white')
+    return image
+
+@st.cache
+def cal_metrics(files, df_input, name2file):
+    for imgfile in files:
+        name2file[imgfile.name] = imgfile
+        image = Image.open(imgfile)
+        uiqm, uciqe = nmetrics(np.array(image))
+        df_input.loc[-1]= [imgfile.name, uiqm, uciqe]
+        df_input.index = df_input.index + 1
+        df_input = df_input.sort_index()
+    return df_input, name2file
+
+
+
+
+st.set_page_config(page_title="Underwater Batch Image Enhancer", page_icon="🤖️", layout ='wide')
+# st.sidebar.header("Underwater Batch Image Enhancer")
+
+left,middle,right = st.columns([1,4,1])
+with middle:
+    st.title("Underwater Batch Image Enhancer")
+    # Define the options for the selection bar
+    options = ['MyModel', 'UDCP', 'GCDP', 'UWCNN','Ucolor']
+    # Create a file uploader widget
+    files = st.file_uploader("Upload an image file", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
+
+_,col1, col2,_ = st.columns([1,2,2,1])
+
+empty_df = pd.DataFrame(columns=['Image Name', 'UIQM', 'UCIQE'])
+for i in range(5):
+    empty_df.loc[-1]=["", "", ""]
+    empty_df.index = empty_df.index + 1
+    empty_df = empty_df.sort_index()
+
+with col1:
+    st.subheader("Input Image")
+    # form = st.form("my_form")
+    placeholder_table1= st.empty()
+    placeholder_table1.table(empty_df)
+    selected_option = st.selectbox("Select an Underwater enhance method :", options, index=0)
+    sumbit_button = st.button("Enhance")
+
+
+with col2:  
+    st.subheader("Enhanced Image")
+    placeholder_table2 = st.empty()
+    placeholder_table2.table(empty_df)
+
+name2file = {}
+if len(files) > 0:
+    # read the image file
+    with col1:
+        with st.spinner("Calculating Metrics..."):
+            df_input = pd.DataFrame(columns=['Image Name', 'UIQM', 'UCIQE'])
+            df_input,name2file = cal_metrics(files, df_input, name2file)
+            placeholder_table1.table(df_input)
+    
+
+if sumbit_button: 
+    with col2:
+        if  'df_input' in locals():
+            # clear the output folder
+            for file in os.listdir('batch_results'):
+                os.remove(os.path.join('batch_results', file))
+            start_time = time.time()
+            df_output = pd.DataFrame(columns=['Image Name', 'UIQM', 'UCIQE'])
+            work_list = df_input['Image Name'].tolist()
+            progress_bar = st.progress(0)
+            for idx, img_name in enumerate(work_list):
+                progress_bar.progress((idx+1)/len(work_list))
+                image = Image.open(name2file[img_name])
+                enhanced_image = enhance_my(image)
+                enhanced_image.save(os.path.join('batch_results', img_name))
+                uiqm, uciqe = nmetrics(np.array(enhanced_image))
+                df_output.loc[-1]= [img_name, uiqm, uciqe]
+                df_output.index = df_output.index + 1
+                df_output = df_output.sort_index()
+                placeholder_table2.table(df_output)
+            end_time = time.time()
+            st.session_state['df_output'] = df_output
+            st.success("ALL images enhanced successfully")
+            st.write("Time taken for processing: ", end_time - start_time, "seconds")
+        else:
+            st.warning("Please upload images to see the enhanced version")
+
+if 'df_output' in st.session_state:
+    placeholder_table2.table(st.session_state['df_output'])
+
+
+
+# open it as a regular file and supply to the button as shown in the example:
+with open("images.zip", "rb") as file:
+    # create the file, from your code
+    outputs = glob.glob('batch_results/*')
+    if len(outputs) > 0:
+        with zipfile.ZipFile("images.zip", "w", zipfile.ZIP_DEFLATED) as z:
+            for idx,image in enumerate(outputs):
+                with open(image, 'rb') as f:
+                    img_data = f.read()
+                    z.writestr(os.path.basename(outputs[idx]),img_data)
+        with col2:
+            btn = st.download_button(
+                    label = "Download Images",
+                    data = file,
+                    file_name = "images.zip",
+                    mime = "application/zip"
+                )
